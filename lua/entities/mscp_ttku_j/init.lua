f5cb302477532657cfad747bc0aa3ae1a8ed8b5a
Lord Mirai (未来) <pilotcraft1001@yahoo.com>
2023-03-03 16:05:11 +0200

    The "thing that kills you" joke SCP

---
+++ lua/entities/mscp_ttku_j/init.lua
@@ -0,0 +1,50 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+function ENT:Initialize()
+	self:SetModel("models/props_phx/construct/metal_tubex2.mdl")
+	self:PhysicsInit(SOLID_VPHYSICS)
+	self:SetMoveType(MOVETYPE_VPHYSICS)
+	self:SetSolid(SOLID_BBOX)
+	self:SetUseType(SIMPLE_USE)
+	self:SetCustomCollisionCheck(true)
+	self:SetTrigger(true)
+
+	self.isSCP = true
+	self.canUse = true
+
+	local phys = self:GetPhysicsObject()
+	if self:IsValid() then self:Activate() end
+	if phys:IsValid() then phys:Wake() end
+end
+
+
+function ENT:Use(ply)
+	if not self.canUse then return end
+
+	if ply:IsPlayer() and ply:Alive() then
+		MSCP.Message(ply, "UHK-class you-have-been-killed scenario has occurred.")
+		MSCP.Message(ply, MSCP.TTKUJ[math.random(#MSCP.TTKUJ)])
+		ply:Kill()
+	end
+
+	self.canUse = false
+	timer.Simple(0.5, function()
+		self.canUse = true
+	end)
+end
+
+function ENT:OnTakeDamage(dmg)
+
+end
+
+
+hook.Add("ShouldCollide", "", function(scp, ent)
+	
+end)
+
+function ENT:StartTouch(otherEnt)
+
+end
---
+++ lua/entities/mscp_ttku_j/shared.lua
@@ -0,0 +1,25 @@
+ENT.Type = "anim"
+ENT.Base = "base_anim"
+
+ENT.PrintName = "SCP-TTKU-J"
+ENT.Category = "Mirai's SCPs"
+ENT.Spawnable = true
+ENT.AdminOnly = true
+
+ENT.Author = "Lord Mirai　(未来)"
+ENT.Purpose = "Kills you"
+ENT.Instructions = "It's a thing that kills you"
+ENT.Contact = "lordmiraithegod@gmail.com | Lord Mirai(未来)#0039"
+
+ENT.Editable = true
+
+MSCP = MSCP or {}
+
+MSCP.TTKUJ = {
+    "SCP-TTKU-J used a weapon to kill you.",
+    "SCP-TTKU-J used a torture device to torture you, then kill you.",
+    "SCP-TTKU-J used its own bare [REDACTED] to kill you.",
+    "SCP-TTKU-J killed you, then continued to kill you.",
+    "SCP-TTKU-J used anomalous methods to kill you",
+    "SCP-TTKU-J killed you"
+}
